5035ab6806b04c6af7a3d762e2859c3069a63634
itzkunal01 <kunal.sharmag12@gmail.com>
2024-02-08 14:20:44 +0530

    updating DeviceDetectionApp ,  method 3 tried

--- src/components/DeviceDetectionApp.jsx
+++ src/components/DeviceDetectionApp.jsx
@@ -1,57 +1,114 @@
 import React from "react";
-// import { isIOS } from "react-device-detect";
+// import { isIOS } from "react-device-detect"; // METHOD 2
 
 const DeviceDetectionApp = () => {
   // ========== METHOD 1 ==========
-  //   React.useEffect(() => {
-  //     const detectDeviceType = () => {
-  //       const isIOSDevice =
-  //         /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  // React.useEffect(() => {
+  //   const detectDeviceType = () => {
+  //     const isIOSDevice =
+  //       /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-  //       if (isIOSDevice) {
-  //         console.log(
-  //           "%cThis is an iOS device.",
-  //           "color: green; font-size: 18px;"
-  //         );
-  //         // OR
-  //         // alert(" this is an iOS device.");
-  //       } else {
-  //         console.log(
-  //           "%cThis is a non-iOS device.",
-  //           "color: green; font-size: 18px;"
-  //         );
-  //         // OR
-  //         //   alert("This is a non-iOS device.");
-  //       }
-  //     };
+  //     if (isIOSDevice) {
+  //       console.log(
+  //         "%cThis is an iOS device, method 1",
+  //         "color: green; font-size: 18px;"
+  //       );
+  //       // OR
+  //       // alert(" this is an iOS device, method 1");
+  //     } else {
+  //       console.log(
+  //         "%cThis is a non-iOS device, method 1",
+  //         "color: green; font-size: 18px;"
+  //       );
+  //       // OR
+  //       //   alert("This is a non-iOS device, method 1");
+  //     }
+  //   };
 
-  //     //   Initial Check On Mount
-  //     detectDeviceType();
-  //   }, []);
+  //   //   Initial Check On Mount
+  //   detectDeviceType();
+  // }, []);
 
   // ========== METHOD 2 ==========
-  //   React.useEffect(() => {
-  //     const detectDeviceType = () => {
-  //       if (isIOS) {
-  //         console.log(
-  //           "%cThis is an iOS device.....",
-  //           "color: green; font-size: 18px;"
-  //         );
-  //         // OR
-  //         // alert("This is an iOS device.");
-  //       } else {
-  //         console.log(
-  //           "%cThis is a non-iOS device.....",
-  //           "color: green; font-size: 18px;"
-  //         );
-  //         // OR
-  //         // alert("This is a non-iOS device.");
-  //       }
-  //     };
+  // React.useEffect(() => {
+  //   const detectDeviceType = () => {
+  //     if (isIOS) {
+  //       console.log(
+  //         "%cThis is an iOS device, method 2",
+  //         "color: green; font-size: 18px;"
+  //       );
+  //       // OR
+  //       // alert("This is an iOS device, method 2");
+  //     } else {
+  //       console.log(
+  //         "%cThis is a non-iOS device, method 2",
+  //         "color: green; font-size: 18px;"
+  //       );
+  //       // OR
+  //       // alert("This is a non-iOS device, method 2");
+  //     }
+  //   };
 
-  //     // Initial Check On Mount
-  //     detectDeviceType();
-  //   }, []);
+  //   // Initial Check On Mount
+  //   detectDeviceType();
+  // }, []);
+
+  // ========== METHOD 3 ==========
+  React.useEffect(() => {
+    const detectDeviceType = () => {
+      const platform = navigator.platform.toLowerCase();
+
+      if (
+        platform.includes("iphone") ||
+        platform.includes("ipad") ||
+        platform.includes("ipod")
+      ) {
+        console.log(
+          "%cThis is an iOS device, method 3",
+          "color: green; font-size: 18px;"
+        );
+        // OR
+        // alert("This is an iOS device, method 3");
+      } else {
+        console.log(
+          "%cThis is a non-iOS device, method 3",
+          "color: green; font-size: 18px;"
+        );
+        // OR
+        // alert("This is a non-iOS device, method 3");
+      }
+    };
+
+    // Initial Check On Mount
+    detectDeviceType();
+  }, []);
+
+  // ========== METHOD 4 ==========
+  // React.useEffect(() => {
+  //   const detectDeviceType = () => {
+  //     const isTouchDevice =
+  //       "ontouchstart" in window || navigator.maxTouchPoints > 0;
+
+  //     if (isTouchDevice) {
+  //       console.log(
+  //         "%cThis is a touch device, method 4",
+  //         "color: green; font-size: 18px;"
+  //       );
+  //       // OR
+  //       // alert("This is a touch device, method 4");
+  //     } else {
+  //       console.log(
+  //         "%cThis is a non-touch device, method 4",
+  //         "color: green; font-size: 18px;"
+  //       );
+  //       // OR
+  //       // alert("This is a non-touch device, method 4");
+  //     }
+  //   };
+
+  //   // Initial Check On Mount
+  //   detectDeviceType();
+  // }, []);
 
   return (
     <>
